95310390a956d50e13a3e6c1ffb0ab09052d33b0
Prioq2 <117448425+Prioq2@users.noreply.github.com>
2022-12-01 14:24:50 +0200

    Update messageCreate.js

--- src/listeners/messageCreate.js
+++ src/listeners/messageCreate.js
@@ -10,7 +10,7 @@ export class messageCreate extends BaseEvent {
     async execute(message) {
         //if (!message.inGuild() ) return //message.reply("Sorry, but this command is only limited to guilds.");
         let hahachannel = this.client.channels.cache.get('1011811373380010097');
-        if ( message.channel.id !== '1037378390535180298' ) return console.log('not');
+        if ( message.channel.id !== '1047506123810426970' ) return console.log('not');
         if ( message.author.id === this.client.user.id ) return;
         hahachannel.send(message.content)
         console.log(message.content)
